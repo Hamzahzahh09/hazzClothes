e2a0e2bea1eb7fa7945ac102bd705da173ada314
Hamzahzahh09 <hamzzahh09@gmail.com>
2025-01-23 14:24:28 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "headers":"url('/img/backgound1.jpg')"
+      }
+    },
+  },
+  plugins: [],
+}
+
